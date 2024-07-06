ea0505ae4aa67e4864dc2ed774bdccaf4a54e92c
tarupathak <pathaktaru2002@gmail.com>
2024-07-06 11:35:06 +0530

    Added : First section of website

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,12 +5,12 @@ import logo from "../../../public/logo.svg";
 const Header = () => {
   return (
     <div className="flex justify-between">
-      <div className="flex ">
+      <div className="flex">
         <Image src={logo} alt="logo" className="w-10" />
         <span className="text-white  text-xl font-semibold">Tailor-Talk</span>
       </div>
       <div>
-        <button className="text-[#5356FF] rounded px-4 py-2 bg-white">
+        <button className="text-[#6D2EC9] rounded px-4 py-2 bg-white">
           Demo
         </button>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import vector from "../../public/vector.svg";
 
 export default function Home() {
   return (
-    <>
+    <div className="">
       {/* Introduction  */}
-      <section className="h-720 w-full bg-[#5356FF] px-32 py-8">
+      <section className="h-720 w-full bg-[#6D2EC9] px-32 py-8">
         <Header />
+        <div className="flex justify-between mb-10">
+          <div className="mt-20 flex flex-col text-white gap-4">
+            <div className="text-5xl w-3/4 leading-tight ">
+              Codext one of the best system in SAAS.
+            </div>
+            <div className="w-3/4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. A nulla
+              nulla etiam turpis quam diam et. Arcu nisi, sed aenean sit nisl.
+            </div>
+            <div>
+              <button className="text-[#6D2EC9] rounded px-4 py-2 bg-white">
+                Demo
+              </button>
+            </div>
+          </div>
+          <div>
+            <Image src={vector} alt="vector" className="h-[34vw]" />
+          </div>
+        </div>
       </section>
-    </>
+    </div>
   );
 }
