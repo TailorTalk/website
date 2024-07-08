84ca671e40ac35f89fe3fc5127e76e2518d2f0c1
tarupathak <pathaktaru2002@gmail.com>
2024-07-08 15:35:42 +0530

    Fixed : Design issues in 1nd section

--- src/app/page.js
+++ src/app/page.js
@@ -41,10 +41,10 @@ export default function Home() {
       <section
         style={{
           backgroundImage: `url(${background.src})`,
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
+          // backgroundRepeat: "no-repeat",
+          // backgroundPosition: "center",
         }}
-        className="px-8 md:px-32 py-8 pb-12 md:pb-20"
+        class="bg-no-repeat h-full w-full px-8 md:px-32 py-8 pb-12 md:pb-20"
       >
         <Header />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-10 mt-8 md:mt-8">
