e267264bd2a7f49d5971398b08f312da2aa92b8e
tarupathak <pathaktaru2002@gmail.com>
2024-07-08 13:50:12 +0530

    Fixed : Graphic location

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="flex justify-center items-center mt-4 md:mt-16">
+          <div className="flex justify-center items-center mt-4 md:mt-0">
             <div className="mt-4 md:mt-16 hidden md:block">
               <Image src={vector1} alt="vector1" layout="responsive" />
             </div>
