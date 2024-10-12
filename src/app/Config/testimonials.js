3764ec7769582304637ad4f67858608e2318a7e8
shiva <shivajbd@gmail.com>
2024-10-12 11:05:07 +0530

    na

--- src/app/Config/testimonials.js
+++ src/app/Config/testimonials.js
@@ -1,45 +1,24 @@
 const testimonials = [
     {
       id: 1,
-      title:"Alice has delivered outstanding results for our business",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "John Doe",
-      role:"CCO & Co-Founder"
+      title:"Leadx has doubled our revenue with ease",
+      text: "Our primary leads came from Instagram ads, but we struggled to scale due to the manual effort required to handle them. With Leadx now managing our Instagram leads, we’ve increased our ad spend and are handling 2.5 times the leads we used to, resulting in a twofold increase in revenue.",
+      author: "Mukta Dharma",
+      role:"Founder - Tootly"
     },
     {
       id: 2,
-      title:"Alice has delivered outstanding results for our business ",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "Jane Smith",
-      role:"CCO & Co-Founder"
+      title:"Leadx turned exhibition visitors into valuable leads",
+      text: "We used Leadx during our exhibition by creating a WhatsApp QR code that allowed visitors to learn about our products easily. Not only did this generate interest at the event, but it also helped us collect contact details of potential customers. Leadx followed up with them afterward, leading to meaningful conversations and new sales opportunities.",
+      author: "Ayappan Radhakrishnan",
+      role:"Sales consultant - CF Tech"
     },
     {
       id: 3,
-      title:"Alice has delivered outstanding results for our business",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "Michael Johnson",
-      role:"CCO & Co-Founder"
-    },
-    {
-      id: 4,
-      title:"Alice has delivered outstanding results for our business",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "Emily Davis",
-      role:"CCO & Co-Founder"
-    },
-    {
-      id: 5,
-      title:"Alice has delivered outstanding results for our business",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "Emily Davis",
-      role:"CCO & Co-Founder"
-    },
-    {
-      id: 6,
-      title:"Alice has delivered outstanding results for our business",
-      text: "Building our outreach strategy with Alice at its core has delivered outstanding results for our business, shifting our sales function from tedious prospect work to spending the majority of our time in new customer meetings.",
-      author: "Emily Davis",
-      role:"CCO & Co-Founder"
+      title:"Leadx improved our appointment booking process significantly",
+      text: "We integrated Leadx with WhatsApp on our website to improve appointment bookings for our eye care hospital. Leadx helps by asking patients the necessary questions, assigning the appropriate doctor, and scheduling the appointment. As soon as an appointment is confirmed, our receptionist receives all the patient details directly on her phone, making the process more efficient for both our staff and patients.",
+      author: "Dr. Shaifali Chahar",
+      role:"Physician - Horus Eye Care"
     }
   ];
   
