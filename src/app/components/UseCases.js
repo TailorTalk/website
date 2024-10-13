c134a8e6336149571d0c06a7ba6847003c8622d6
shiva <shivajbd@gmail.com>
2024-10-13 21:31:48 +0530

    na

--- src/app/components/UseCases.js
+++ src/app/components/UseCases.js
@@ -15,9 +15,9 @@ const UseCases = () => {
   return (
     <div className="w-11/12 px-6 md:px-12 lg:px-24 pt-12 bg-[#f1f1ee] rounded-3xl mx-auto">
       <h2 className="text-3xl md:text-5xl font-medium text-gray-900 text-center mt-5 mb-6">Use cases</h2>
-      <p className="text-sm md:text-base text-gray-500 text-center md:mb-0 mb-5">See how we can transform your customer interactions with a single call.</p>
+      <p className="text-sm md:text-xl text-gray-500 text-center md:mb- mb-6">See how we can transform your customer interactions with a single call.</p>
       
-      <div className="flex flex-col md:flex-row md:pt-28 w-full">
+      <div className="flex flex-col md:flex-row md:pt-16 w-full">
         <div className="w-full md:w-1/2 pr-4 mb-10 md:mb-0">
           <div className="flex flex-col gap-4 justify-center items-center">
             {Header.map((item) => (
@@ -46,10 +46,10 @@ const UseCases = () => {
           {selectedHeader >= 1 && (
             <div className="flex flex-col w-full h-auto md:h-[30rem] p-4 overflow-hidden">
               <div className="w-full md:pl-10 flex flex-col justify-center">
-                <h3 className="text-2xl md:text-4xl font-medium text-gray-900 mb-2">
+                <h3 className="text-2xl md:text-4xl font-medium text-gray-900 mb-4" style={{ lineHeight: '1.5' }}>
                   {useCaseData[selectedHeader - 1].title}
                 </h3>
-                <p className="text-gray-600 text-base md:text-lg mt-6">
+                <p className="text-gray-600 text-base md:text-l mt-6" style={{ lineHeight: '1.6' }}>
                   {useCaseData[selectedHeader - 1].description}
                 </p>
               </div>
