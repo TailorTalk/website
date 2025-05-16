fb02568ba0aef30adcf80b1860b00b6e5ff83f5b
shiva <shivajbd@gmail.com>
2025-05-16 15:22:14 +0530

    na

--- src/app/components/ToolsIntegration.js
+++ src/app/components/ToolsIntegration.js
@@ -99,7 +99,7 @@ export default function SeamlessIntegrations() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8 w-[70%]">
       <div className="text-center mb-12">
-        <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 mb-5">
+        <h2 className="text-3xl md:text-4xl font-semibold text-gray-900 mb-5">
           Seamless <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">Integrations</span>
         </h2>
         <p className="text-[#61646b] text-[17px] max-w-2xl mx-auto">
